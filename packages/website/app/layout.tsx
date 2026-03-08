711b2cc82a2b286bc958c22e7998e8d0f618a320
Roman Hauksson-Neill <62364808+RomanHauksson@users.noreply.github.com>
2026-03-08 14:05:36 -0500

    configure homepage packages

---
+++ packages/website/app/layout.tsx
@@ -0,0 +1,35 @@
+import { Noto_Sans, Noto_Sans_Mono } from "next/font/google";
+
+import "@template/app/globals.css";
+import { ThemeProvider } from "@template/components/theme-provider";
+import { cn } from "@template/lib/utils";
+
+const notoSans = Noto_Sans({ variable: "--font-sans" });
+
+const fontMono = Noto_Sans_Mono({
+  subsets: ["latin"],
+  variable: "--font-mono",
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      suppressHydrationWarning
+      className={cn(
+        "antialiased",
+        fontMono.variable,
+        "font-sans",
+        notoSans.variable,
+      )}
+    >
+      <body>
+        <ThemeProvider>{children}</ThemeProvider>
+      </body>
+    </html>
+  );
+}
