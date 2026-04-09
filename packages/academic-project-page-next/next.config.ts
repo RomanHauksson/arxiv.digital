d4a29d4043e89b9459ac3996c4969f66bef1a24c
Roman Hauksson-Neill <62364808+RomanHauksson@users.noreply.github.com>
2026-04-08 21:31:02 -0500

    Use Expressive Code for codeblocks

--- packages/academic-project-page-next/next.config.ts
+++ packages/academic-project-page-next/next.config.ts
@@ -8,7 +8,7 @@ const nextConfig: NextConfig = {
 const withMDX = createMDX({
 	options: {
 		remarkPlugins: ["remark-gfm"],
-		rehypePlugins: [],
+		rehypePlugins: ["rehype-expressive-code"],
 	},
 });
 
