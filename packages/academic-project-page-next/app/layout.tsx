6eb5a0cbe22e5479cac5c0f74ee69a06d04fd1c4
Roman Hauksson-Neill <62364808+RomanHauksson@users.noreply.github.com>
2026-04-08 23:03:48 -0500

    simplify theme management

--- packages/academic-project-page-next/app/layout.tsx
+++ packages/academic-project-page-next/app/layout.tsx
@@ -1,7 +1,6 @@
 import { Noto_Sans, Noto_Sans_Mono } from "next/font/google";
 
 import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider";
 import { cn } from "@/lib/utils";
 
 const notoSans = Noto_Sans({ variable: "--font-sans" });
@@ -17,33 +16,29 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html
-			lang="en"
-			suppressHydrationWarning
-			className={cn(
-				"antialiased",
-				fontMono.variable,
-				"font-sans",
-				notoSans.variable,
-			)}
-		>
-			<body className="prose prose-lg prose-zinc dark:prose-invert prose-a:text-blue-500 prose-a:dark:text-blue-300 prose-a:no-underline prose-a:hover:underline prose-a:font-normal prose-code:bg-zinc-200 prose-code:dark:bg-zinc-800 prose-code:text-zinc-800 prose-code:dark:text-zinc-200 prose-code:px-1.5 prose-code:py-0.5 prose-code:rounded prose-code:font-medium prose-code:before:content-none prose-code:after:content-none max-w-none pt-16 pb-6">
-				<ThemeProvider>
-					<main>
-						{children}
-						<footer className="mx-auto max-w-[50rem] px-6 text-center mt-12">
-							<p className="text-sm text-center text-muted-foreground">
-								Built with{" "}
-								<a
-									href="https://github.com/RomanHauksson/academic-project-astro-template"
-									rel="nofollow"
-								>
-									Roman Hauksson-Neill&apos;s project page template
-								</a>
-							</p>
-						</footer>
-					</main>
-				</ThemeProvider>
+		<html lang="en">
+			<body
+				className={cn(
+					"antialiased font-sans",
+					fontMono.variable,
+					notoSans.variable,
+					"prose prose-lg prose-zinc dark:prose-invert prose-a:text-blue-500 prose-a:dark:text-blue-300 prose-a:no-underline prose-a:hover:underline prose-a:font-normal prose-code:bg-zinc-200 prose-code:dark:bg-zinc-800 prose-code:text-zinc-800 prose-code:dark:text-zinc-200 prose-code:px-1.5 prose-code:py-0.5 prose-code:rounded prose-code:font-medium prose-code:before:content-none prose-code:after:content-none max-w-none pt-16 pb-6",
+				)}
+			>
+				<main>
+					{children}
+					<footer className="mx-auto max-w-[50rem] px-6 text-center mt-12">
+						<p className="text-sm text-center text-muted-foreground">
+							Built with{" "}
+							<a
+								href="https://github.com/RomanHauksson/academic-project-astro-template"
+								rel="nofollow"
+							>
+								Roman Hauksson-Neill&apos;s project page template
+							</a>
+						</p>
+					</footer>
+				</main>
 			</body>
 		</html>
 	);
